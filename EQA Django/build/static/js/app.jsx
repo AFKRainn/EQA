3ed70b79bc81835a07d766be538d0919ffb8eba5
Rane Deeb <88451666+AFKRainn@users.noreply.github.com>
2023-11-23 19:35:11 +0200

    Added Project

---
+++ EQA Django/build/static/js/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function MyComponent() {
+    return <h1>Hello, world!</h1>;
+  }
+  
+  // Render the component to the DOM
+  ReactDOM.render(<MyComponent />, document.getElementById('root'));
