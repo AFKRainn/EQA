6bbb38b4df0fae452831940599a3de0034aab4bd
Rane Deeb <88451666+AFKRainn@users.noreply.github.com>
2023-11-24 19:16:59 +0200

    Finished React Setup

--- EQA Django/build/static/js/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-function MyComponent() {
-    return <h1>Hello, world!</h1>;
-  }
-  
-  // Render the component to the DOM
-  ReactDOM.render(<MyComponent />, document.getElementById('root'));
